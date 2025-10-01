c9ae8df398ab78159564ee5b56c7aaf5bf7f3f7a
Angus H Wright <awright@astro.rub.de>
2025-10-01 13:10:28 +0000

    update equalN binnning for weighted and limits only modes

--- scripts/equalN_split.R
+++ scripts/equalN_split.R
@@ -3,7 +3,7 @@
 # File Name : spatial_split.R
 # Created By : awright
 # Creation Date : 10-07-2023
-# Last Modified : Mon Aug 25 11:41:49 2025
+# Last Modified : Sat Jul 26 14:01:57 2025
 #
 #=========================================
 
@@ -14,7 +14,8 @@ inputs<-commandArgs(TRUE)
 
 #Interpret the command line options {{{
 badval<- -999
-ID_only<-FALSE
+limits.only<-FALSE
+w.name='none'
 while (length(inputs)!=0) {
   #Check for valid specification {{{
   while (length(inputs)!=0 && inputs[1]=='') { inputs<-inputs[-1] }  
@@ -55,10 +56,16 @@ while (length(inputs)!=0) {
     x.name<-inputs[1]
     inputs<-inputs[-1]
     #/*fold*/}}}
-  } else if (inputs[1]=='--id_only') { 
-    #Return ids only /*fold*/ {{{
+  } else if (inputs[1]=='-w') { 
+    #Read the weight column names /*fold*/ {{{
     inputs<-inputs[-1]
-    ID_only<-TRUE
+    w.name<-inputs[1]
+    inputs<-inputs[-1]
+    #/*fold*/}}}
+  } else if (inputs[1]=='--limits.only') { 
+    #Read the output catalogue(s) /*fold*/ {{{
+    inputs<-inputs[-1]
+    limits.only<-TRUE
     #/*fold*/}}}
   } else {
     stop(paste("Unknown option",inputs[1]))
@@ -67,24 +74,30 @@ while (length(inputs)!=0) {
 #}}}
 
 #Check that length of the output files is correct {{{
-if (!ID_only) { 
-  if (length(output.cats)!=nsplit) { 
-    stop("Output file list must be of length nsplit") 
-  } 
-}
+if (length(output.cats)!=nsplit & !limits.only) { 
+  stop("Output file list must be of length nsplit") 
+} 
 #}}}
 
 #Read the input catalogue for col names {{{
-cols<-helpRfuncs::read.colnames(input.cat)
+cat<-helpRfuncs::read.colnames(input.cat)
 #}}}
 
 #Check that the x.name varaible is in the catalogue 
-if (!x.name %in% cols) {  
+if (!x.name %in% cat) {  
   stop(paste(x.name,"variable is not in provided catalogue!")) 
 }
+weighted<-TRUE
+if (!w.name %in% cat) {  
+  weighted=FALSE
+  warning(paste(w.name,"variable is not in provided catalogue! will perform unweighted split.")) 
+}
+
 
 #Read the input catalogue {{{
-if (ID_only) { 
+if (limits.only & weighted) { 
+  cat<-helpRfuncs::read.file(input.cat,cols=c(x.name,w.name))
+} else if (limits.only) { 
   cat<-helpRfuncs::read.file(input.cat,cols=x.name)
 } else { 
   cat<-helpRfuncs::read.file(input.cat)
@@ -92,7 +105,11 @@ if (ID_only) {
 #}}}
 
 #Set up the cut object (for faster splitting) {{{
-tmp<-data.frame(x=cat[[x.name]])
+if (weighted) { 
+  tmp<-data.frame(x=cat[[x.name]],w=cat[[w.name]])
+} else { 
+  tmp<-data.frame(x=cat[[x.name]])
+}
 #}}}
 
 #X range {{{
@@ -106,37 +123,62 @@ if (any(tmp$x==badval)) {
 
 cat(paste0("x.range: ",'[',x.range[1],',',x.range[2],']\n'))
 
-#Create bins {{{
-if (nsplit==1) { 
-  #There is only one bin?! {{{
-  cat('Only one bin!\n')
-  bins<-rep(1,nrow(tmp))
-  #}}}
-} else {
-  #Bin in x & y {{{
+if (limits.only) { 
+  #Compute and output the equal n limits {{{
   if (any(tmp$x==badval)) { 
-    cat('binning in x (with good values only)!\nEntries with bad x values are randomly assigned to a bin!')
-    bins<-rep(NA,nrow(tmp)) 
-    good<-tmp$x!=badval
-    bins[good]<-with(tmp[good,],tapply(1:length(x),list(x=cut(x, breaks=quantile(x,probs=seq(0,1,length=nsplit+1)), include.lowest=T))))
-    bins[!good]<-sample(1:nsplit,size=length(which(!good)),replace=T)
+    cat('defining limits with good values only!\n')
+    if (weighted) { 
+      lims=reldist::wtd.quantile(tmp$x[which(tmp$x!=badval)],q=seq(0,1,length=nsplit+1),weight=tmp$w[which(tmp$x!=badval)])
+    } else { 
+      lims=quantile(tmp$x[which(tmp$x!=badval)],probs=seq(0,1,length=nsplit+1))
+    }
   } else { 
-    cat('binning in x\n')
-    bins<-with(tmp,tapply(1:nrow(tmp),list(x=cut(x, breaks=quantile(x,probs=seq(0,1,length=nsplit+1)), include.lowest=T))))
+    if (weighted) { 
+      lims=reldist::wtd.quantile(tmp$x,q=seq(0,1,length=nsplit+1),weight=tmp$w)
+    } else { 
+      lims=quantile(tmp$x,probs=seq(0,1,length=nsplit+1))
+    } 
   } 
+  #Write the limits to file 
+  helpRfuncs::write.file(file=output.cats[1],rbind(lims))
   #}}}
-}
-#}}}
-
-cat('bin occupation:\n')
-print(table(bins))
-
-#For each split, output the catalogue {{{
-if (ID_only) { 
-  out<-cat
-  out$splitvar<-bins
-  helpRfuncs::write.file(file=output.cats[1],out)
 } else { 
+  #Create bins {{{
+  if (nsplit==1) { 
+    #There is only one bin?! {{{
+    cat('Only one bin!\n')
+    bins<-rep(1,nrow(tmp))
+    #}}}
+  } else {
+    #Bin in x & y {{{
+    if (any(tmp$x==badval)) { 
+      cat('binning in x (with good values only)!\nEntries with bad x values are randomly assigned to a bin!')
+      bins<-rep(NA,nrow(tmp)) 
+      good<-tmp$x!=badval
+      if (weighted) { 
+        breaks=reldist::wtd.quantile(tmp$x[good],q=seq(0,1,length=nsplit+1),weight=tmp$w[good])
+      } else { 
+        breaks=quantile(tmp$x[good],probs=seq(0,1,length=nsplit+1))
+      } 
+      bins[good]<-with(tmp[good,],tapply(1:length(x),list(x=cut(x, breaks=breaks, include.lowest=T))))
+      bins[!good]<-sample(1:nsplit,size=length(which(!good)),replace=T)
+    } else { 
+      cat('binning in x\n')
+      if (weighted) { 
+        breaks=reldist::wtd.quantile(tmp$x,q=seq(0,1,length=nsplit+1),weight=tmp$w)
+      } else { 
+        breaks=quantile(tmp$x,probs=seq(0,1,length=nsplit+1))
+      } 
+      bins<-with(tmp,tapply(1:nrow(tmp),list(x=cut(x, breaks=breaks, include.lowest=T))))
+    } 
+    #}}}
+  }
+  #}}}
+
+  cat('bin occupation:\n')
+  print(table(bins))
+
+  #For each split, output the catalogue {{{
   written<-FALSE
   for (i in seq(1,nsplit)) { 
     #Select the relevant sources {{{
@@ -144,7 +186,7 @@ if (ID_only) {
     #}}}
     cat(paste('bin',i,x.name,'stats:\n'))
     print(summary(out[[x.name]]))
-  
+
     if (nrow(out)==0) { 
       cat(paste("WARNING: split",i,"contains no sources?!\n"))
     } else { 
@@ -157,8 +199,8 @@ if (ID_only) {
   if (!written) { 
     stop("Nothing was written to disk?!") 
   }
+  #}}}
 }
-#}}}
 
 #Finish
 

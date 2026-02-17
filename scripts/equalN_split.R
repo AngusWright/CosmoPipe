1dd7b224de3f26a2375b55b83f07da8ff8f26a3a
Angus H Wright <awright@astro.rub.de>
2026-02-17 09:32:56 +0000

    make BIN_ID compatible with use in extract_patch

--- scripts/equalN_split.R
+++ scripts/equalN_split.R
@@ -3,7 +3,7 @@
 # File Name : spatial_split.R
 # Created By : awright
 # Creation Date : 10-07-2023
-# Last Modified : Tue Feb 17 09:25:55 2026
+# Last Modified : Tue Feb 17 09:32:00 2026
 #
 #=========================================
 
@@ -183,7 +183,7 @@ if (limits.only) {
   for (i in seq(1,nsplit)) { 
     #Select the relevant sources {{{
     out<-cat[which(bins==i),]
-    out$BIN_ID<-i
+    out$BIN_ID<-paste0(i,"_patch")
     #}}}
     cat(paste('bin',i,x.name,'stats:\n'))
     print(summary(out[[x.name]]))
